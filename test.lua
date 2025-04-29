7d8766f51b9e05f0374dc3feda3cde2a89c2289f
UnrelatedString <unrelatedstring@gmail.com>
2025-04-29 03:30:05 -0400

    ...so backslash paren has a special non escape meaning in sed regexes??? uh oh

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
 -- fix for compiler typo
 os.execute("sed 's/patch_v = patches/patch_v = patches[k]/' output/test.lua > output/test_.lua")
 -- I just have no idea what library this is from because it's inlined LMAO
-os.execute("sed 's/for str in s:gmatch\\(\"\\(\\[\\^\" \\.\\. sep \\.\\. \"\\]\\+\\)\"\\)/local pattern; if string.len(sep) ~= 0 then pattern = \"([^\" .. sep .. \"]+)\" else pattern = \".\" end; for str in s:gmatch(pattern)/' output/test_.lua > output/test__.lua")
+os.execute("sed 's/for str in s:gmatch.+\\\\)/local pattern; if string.len(sep) ~= 0 then pattern = \"([^\" .. sep .. \"]+)\" else pattern = \".\" end; for str in s:gmatch(pattern)/' output/test_.lua > output/test__.lua")
 dofile("output/test__.lua").main()
