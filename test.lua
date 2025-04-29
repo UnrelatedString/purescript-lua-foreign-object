17c91deeabba0b4dfd8e989a7a27fd7ac37512fa
UnrelatedString <unrelatedstring@gmail.com>
2025-04-29 03:46:50 -0400

    WHATTTSKG:LSAGKMAGEKGR:EQRGK

--- test.lua
+++ test.lua
@@ -1,3 +1,3 @@
 -- fix for compiler typo
 os.execute("sed 's/patch_v = patches/patch_v = patches[k]/' output/test.lua > output/test_.lua")
-dofile("output/test__.lua").main()
+dofile("output/test_.lua").main()
