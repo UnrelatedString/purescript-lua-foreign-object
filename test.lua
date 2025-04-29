c90be8cb59192ef195c9de0ca55119eadf83b8c4
UnrelatedString <unrelatedstring@gmail.com>
2025-04-29 03:25:52 -0400

    aaahahahahaha yeah i dont know how to use sed tbh:

--- test.lua
+++ test.lua
@@ -1,3 +1,5 @@
 -- fix for compiler typo
 os.execute("sed 's/patch_v = patches/patch_v = patches[k]/' output/test.lua > output/test_.lua")
-dofile("output/test_.lua").main()
+-- I just have no idea what library this is from because it's inlined LMAO
+os.execute("sed 's/for str in s:gmatch(\"([^\" .. sep .. \"]+)\") do table.insert(t, str) end/local pattern; if string.len(sep) ~= 0 then pattern = \"([^\" .. sep .. \"]+)\" else pattern = \".\" end; for str in s:gmatch(pattern) do table.insert(t, str) end/' output/test_.lua > output/test__.lua")
+dofile("output/test__.lua").main()
